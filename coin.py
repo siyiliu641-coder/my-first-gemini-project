75eed3a39561cf7f50900c90c299172759eb2359
siyiliu641-coder <siyiliu641@gmail.com>
2025-12-04 05:37:10 +0800

    Create coin.py

---
+++ coin.py
@@ -0,0 +1,10 @@
+import random
+coin = ['正面', '反面']
+print("正在抛硬币...")
+print("3...")
+print("2...")
+print("1...")
+result = random.choice(coin)
+print("----------------")
+print("结果是：" + result)
+print("----------------")
